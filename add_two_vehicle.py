8a2924305e4e35c95f16b6e748e30d29f115ec2e
Andrew Murphy <acm7552g.rit.edu>
2025-09-30 19:35:25 -0400

    first WIP commit adding functions to icp

--- add_two_vehicle.py
+++ add_two_vehicle.py
@@ -29,7 +29,7 @@ def main():
     actor_list = []
     try:
         client = carla.Client('localhost', 2000)
-        client.set_timeout(2.0)
+        client.set_timeout(30.0)
         world = client.get_world()
 
         blueprint_library = world.get_blueprint_library()
